9fabb14d20dfd0d71bd3d170161ca487ace2ee3a
RittikSoni <kingrittiksoni@gmail.com>
2026-02-06 23:34:22 +0530

    fix: add test stubs with main() functions for CI

--- apps/kr_ui_docs/test/widget_test.dart
+++ apps/kr_ui_docs/test/widget_test.dart
@@ -1,1 +1,9 @@
+import 'package:flutter_test/flutter_test.dart';
 
+void main() {
+  testWidgets('kr_ui_docs widget tests coming soon',
+      (WidgetTester tester) async {
+    // Widget tests will be added soon!
+    expect(true, isTrue);
+  });
+}
--- packages/kr_ui/test/kr_ui_test.dart
+++ packages/kr_ui/test/kr_ui_test.dart
@@ -1,2 +1,9 @@
-// coming soon! Stay tuned!
-// want to contribute? check out the contributing.md file
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('kr_ui package tests coming soon', () {
+    // Tests will be added soon!
+    // Want to contribute? Check out CONTRIBUTING.md
+    expect(true, isTrue);
+  });
+}
